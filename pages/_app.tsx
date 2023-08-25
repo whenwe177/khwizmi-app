78e9dab323d5f0918bbdc05e9423e1f71c25ac02
alexander.caesario <alexandercaesariobangun@gmail.com>
2023-08-25 18:16:15 +1000

    my last long commit message

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
-import '@/styles/globals.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import type { AppProps } from 'next/app'
-import { useState } from 'react'
+import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { AppProps } from "next/app";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
-  return <QueryClientProvider client={queryClient}>
-
-  <Component {...pageProps} />
-  </QueryClientProvider> 
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
