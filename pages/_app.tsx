f5627fa605af1d8b458e18c93ac6e3473e09aa1f
Rashad Aziz <rashad.aziz@ui.ac.id>
2023-08-25 23:26:06 +1000

    fix: add worker for pdf viewer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import { useEffect, useState } from "react";
 import * as pdfjsLib from 'pdfjs-dist'
+import { pdfjs } from "react-pdf";
 
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
   useEffect(() => {
     pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjsLib.version}/build/pdf.worker.js`;
+    pdfjs.GlobalWorkerOptions.workerSrc = `//cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/build/pdf.worker.js`
   }, []);
   return (
     <QueryClientProvider client={queryClient}>
