7f702c408de0a977b59cfc6d26f5c50485d35103
alexander.caesario <alexandercaesariobangun@gmail.com>
2023-08-25 20:22:40 +1000

    rashad is a legend

--- pages/components/PdfUpload.tsx
+++ pages/components/PdfUpload.tsx
@@ -1,30 +1,30 @@
-import { storage } from "@/firebase";
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
-import { useState } from "react";
-
-import {pdfjs} from "react-pdf";
-
-// import pdfJsLib from "pdfjs-dist";
+import { useEffect, useRef, useState } from "react";
+import * as pdfjsLib from "pdfjs-dist";
 
 const PdfUploader = () => {
-  pdfjs.GlobalWorkerOptions.workerSrc =
-	`https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.js`;
-  const [fileUpload, setFileUpload] = useState<File | null>(null);
-  const [fileUrl, setFileUrl] = useState("");
+    const [fileUpload, setFileUpload] = useState<File | null>(null);
+    const fileReader = useRef<FileReader>();
+    useEffect(() => {
+        pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjsLib.version}/build/pdf.worker.js`;
+        fileReader.current = new FileReader();
+    }, [])
 
   const onFormSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
-    if (fileUpload == null) return;
-    const fileFolderPath = `projectFiles/${fileUpload.name}`;
-    const fileFolderRef = ref(storage, fileFolderPath);
-
-    try {
-      await uploadBytes(fileFolderRef, fileUpload);
-      const url = await getDownloadURL(ref(storage, fileFolderPath));
-      setFileUrl(url);
-    } catch (error) {
-      console.error(error);
+    
+    if (fileUpload == null) {
+        console.log("Upload smth");
+        return;
     }
+    console.log("First")
+    const buffer = await fileUpload?.arrayBuffer();
+    console.log("Works")
+    const loadingTask = pdfjsLib.getDocument(buffer!);
+    
+    const pdf = await loadingTask.promise
+    const page = await pdf.getPage(2);
+    const result = await page.getTextContent();
+    const resultString = result.items.map((item: any) => item.str).join(" ")
   };
 
   const parsePDF = async () => {
