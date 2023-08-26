71c299fab35206ac5437fcd5aa001d21944be9c6
alexander.caesario <alexandercaesariobangun@gmail.com>
2023-08-26 10:26:47 +1000

    svg change to be better

--- components/Svg/Upload.tsx
+++ components/Svg/Upload.tsx
@@ -18,9 +18,9 @@ const Upload: React.FC<Props> = ({height, width, color}) => {
       <path
         d="M47.5 32.5V42.5C47.5 43.8261 46.9732 45.0979 46.0355 46.0355C45.0979 46.9732 43.8261 47.5 42.5 47.5H7.5C6.17392 47.5 4.90215 46.9732 3.96447 46.0355C3.02678 45.0979 2.5 43.8261 2.5 42.5V32.5M37.5 15L25 2.5M25 2.5L12.5 15M25 2.5V32.5"
         stroke={color}
-        stroke-width="3.75"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="3.75"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
