d72b998e49181f1d51a78809339b97697110996b
Rashad Aziz <rashad.aziz@ui.ac.id>
2023-08-26 22:47:51 +1000

    fix: create more deterministic api

--- pages/api/khwizify.ts
+++ pages/api/khwizify.ts
@@ -22,46 +22,85 @@ export default async function handler(
           "You are an expert quiz maker who can create effective quizzes that test the knowledge of students about any material",
       },
       {
-        role: "system",
-        content:
-          "I am going to send you material prefixed with 'CONTEXT:' and you are expected to create the quiz based on the given material",
+        role: "user",
+        content: `MATERIAL:${context}`,
       },
       {
         role: "user",
-        content: `CONTEXT:${context}`,
+        content: `Analyze the material above and generate a quiz consisting a maximum of 15 question`,
       },
+    ],
+    functions: [
       {
-        role: "user",
-        content: `Please generate a quiz, which is an array of JSON objects each with the following structure:
-      [
-        {
-          "question": // string (the question should not be too hard or too easy, try using synonyms)
-          "expected_duration": // number (how long you think (in seconds) the question should take to solve)
-          "choices": { // (make sure the choices are concise and not obvious)
-                  "a": // string,
-                  "b": // string,
-                  "c": // string,
-                  "d": // string,
-                }
-           "answer": {
-             correct_choice: // string (either "a", "b", "c", or "d")
-                reason: // string (give the reason on why the correct choice is indeed correct)
-            }
+        name: "do_quiz",
+        parameters: {
+          type: "object",
+          properties: {
+            quiz: {
+              type: "array",
+              items: {
+                type: "object",
+                properties: {
+                  question: {
+                    type: "string",
+                    description:
+                      "Question based on the material that is not too easy and uses synonyms. Hard questions are preferred",
+                  },
+                  expected_duration: {
+                    type: "number",
+                    description:
+                      "A generous expected duration needed to complete the question in seconds with a minimum of 25 seconds",
+                  },
+                  choices: {
+                    type: "object",
+                    description: "Object containing choices A, B, C, and D. Compound choices are also allowed, for example, Both A and B",
+                    properties: {
+                      a: {
+                        type: "string",
+                        description: "Possible answer A for the question",
+                      },
+                      b: {
+                        type: "string",
+                        description: "Possible answer B for the question",
+                      },
+                      c: {
+                        type: "string",
+                        description: "Possible answer C for the question",
+                      },
+                      d: {
+                        type: "string",
+                        description: "Possible answer D for the question",
+                      },
+                    },
+                  },
+                  answer: {
+                    type: "object",
+                    properties: {
+                      correct_choice: {
+                        type: "string",
+                        description: "The correct answer: A, B, C, or, D",
+                      },
+                      reason: {
+                        type: "string",
+                        description:
+                          "A comprehensive reason on why the correct answer was indeed correct",
+                      },
+                    },
+                  },
+                },
+              },
+              description: "An array consisting a maximum of 15 questions",
+            },
+          },
         },
-        ...
-      ]
-      Return the array ONLY, and generate at most 15 questions
-        `,
       },
-      { role: "assistant", content: "Sure! Here's your array of JSON objects" },
     ],
     model: "gpt-3.5-turbo",
-    n: 1,
   });
 
-  return res
-    .status(200)
-    .json(JSON.parse(completion.choices[0].message.content || ""));
+  const quiz = JSON.parse(completion.choices[0].message.function_call?.arguments!)
+
+  return res.status(200).json(quiz.quiz);
   return res
     .status(200)
     .json(
