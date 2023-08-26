e3061046d71ecd0e013eac9ab2c2a0e99603a36d
Rashad Aziz <rashad.aziz@ui.ac.id>
2023-08-26 10:24:48 +1000

    feat: add user to context

--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -5,9 +5,12 @@ import {
   createContext,
   useState,
   useContext,
+  useEffect
 } from "react";
-
+import { auth } from "@/firebase";
+import { User, onAuthStateChanged } from "firebase/auth";
 interface AppContextState {
+  user: User | null;
   content: string;
   setContent: Dispatch<SetStateAction<string>>;
 }
@@ -16,8 +19,16 @@ const AppContext = createContext<AppContextState>({} as AppContextState);
 
 const AppProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const [content, setContent] = useState("");
+  const [user, setUser] = useState<User | null>(null);
+  
+  useEffect(() => {
+    return onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    })
+  }, [])
+
   return (
-    <AppContext.Provider value={{ content, setContent }}>
+    <AppContext.Provider value={{ user, content, setContent }}>
       {children}
     </AppContext.Provider>
   );
