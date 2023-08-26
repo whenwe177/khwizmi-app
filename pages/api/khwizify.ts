9c503277a3feabc200a8e3a94a02a3415f543cb1
Rashad Aziz <rashad.aziz@ui.ac.id>
2023-08-26 21:18:24 +1000

    fix api

--- pages/api/khwizify.ts
+++ pages/api/khwizify.ts
@@ -1,12 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import OpenAI from 'openai';
+import type { NextApiRequest, NextApiResponse } from "next";
+import OpenAI from "openai";
 
 type Data = {
-  quiz: string
-}
+  quiz: string;
+};
 
-// const openai = new OpenAI();
+const openai = new OpenAI();
 
 export default async function handler(
   req: NextApiRequest,
@@ -14,35 +14,59 @@ export default async function handler(
 ) {
   const { context } = JSON.parse(req.body);
 
-  // const completion = await openai.chat.completions.create({
-  //   messages: [
-  //     {
-  //       role: "user",
-  //       content: `You are an expert quiz maker who is able to create quizzes that effectively tests the knowledge of learners. Your job is to create a quiz and return each question in a JSON format with the following structure
-  //     {
-  //       expected_duration: ...,
-  //       question: ...,
-  //       choices: {
-  //         a: ...,
-  //         b: ...,
-  //         c: ...,
-  //         d: ...,
-  //       }
-  //       answer: {
-  //         correct_choice: ...,
-  //         reason: ...
-  //       }
-  //     }. The answer property will consist of correct_choice and reason. You will provide the correct choice ('a', 'b', 'c', 'd') in the correct_choice property whilst also providing the reason on why it is correct. Each choice should be concise. Depending on how hard the question is give an expected_duration in seconds. You will be given a block of text as context, prefixed with 'CONTEXT:'. Create a list of questions based on the text and return a maximum of 15 questions for the quiz. Return the list of questions only and no other text.`,
-  //     },
-  //     {
-  //       role: "user",
-  //       content: `CONTEXT:${context}`,
-  //     },
-  //   ],
-  //   model: "gpt-3.5-turbo",
-  //   n: 1,
-  // });
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are an expert quiz maker who can create effective quizzes that test the knowledge of students about any material",
+      },
+      {
+        role: "system",
+        content:
+          "I am going to send you material prefixed with 'CONTEXT:' and you are expected to create the quiz based on the given material",
+      },
+      {
+        role: "user",
+        content: `CONTEXT:${context}`,
+      },
+      {
+        role: "user",
+        content: `Please generate a quiz, which is an array of JSON objects each with the following structure:
+      [
+        {
+          "question": // string (the question should not be too hard or too easy, try using synonyms)
+          "expected_duration": // number (how long you think (in seconds) the question should take to solve)
+          "choices": { // (make sure the choices are concise and not obvious)
+                  "a": // string,
+                  "b": // string,
+                  "c": // string,
+                  "d": // string,
+                }
+           "answer": {
+             correct_choice: // string (either "a", "b", "c", or "d")
+                reason: // string (give the reason on why the correct choice is indeed correct)
+            }
+        },
+        ...
+      ]
+      Return the array ONLY, and generate at most 15 questions
+        `,
+      },
+      { role: "assistant", content: "Sure! Here's your array of JSON objects" },
+    ],
+    model: "gpt-3.5-turbo",
+    n: 1,
+  });
 
-  // return res.status(200).json(JSON.parse(completion.choices[0].message.content || ''))
-  return res.status(200).json(JSON.parse('[{"expected_duration": 60, "question": "Indonesia?", "choices": {"a": "Yes", "b": "Yes","c": "Yes","d": "Yes"}, "answer": {"correct_choice": "a", "reason":"Yes"}}, {"expected_duration": 60, "question": "Indonesian?", "choices": {"a": "Yes", "b": "Yes","c": "Yes","d": "Yes"}, "answer": {"correct_choice": "a", "reason":"Yes"}}]'))
+  return res
+    .status(200)
+    .json(JSON.parse(completion.choices[0].message.content || ""));
+  return res
+    .status(200)
+    .json(
+      JSON.parse(
+        '[{"expected_duration": 60, "question": "Indonesia?", "choices": {"a": "Yes", "b": "Yes","c": "Yes","d": "Yes"}, "answer": {"correct_choice": "a", "reason":"Yes"}}, {"expected_duration": 60, "question": "Indonesian?", "choices": {"a": "Yes", "b": "Yes","c": "Yes","d": "Yes"}, "answer": {"correct_choice": "a", "reason":"Yes"}}]'
+      )
+    );
 }
