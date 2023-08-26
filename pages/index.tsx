bb9542af5fd8d8a992cb836a6aae304d37264176
Rashad Aziz <rashad.aziz@ui.ac.id>
2023-08-26 17:13:49 +1000

    feat: shadcn/ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import Logo from "@/components/Logo";
+import React from "react";
+import Image from "next/image";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <main
+      style={{
+        background: "linear-gradient(180deg, #0E032F 0%, #283472 100%)",
+      }}
+      className="min-h-screen w-full overflow-auto"
+    >
+      <div className="h-40 mt-52 flex items-center justify-center gap-4">
+        <Logo className="h-[80px] w-[82px]" />
+        <h1 className="text-white text-7xl font-semibold">Khwizmi</h1>
+      </div>
+      <div
+        className="w-full h-screen absolute top-0 pointer-events-none"
+        style={{
+          background: 'url("bg1.png")',
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPositionY: "150px",
+        }}
+      />
+    </main>
+  );
+};
 
-export default Home
+export default Home;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,85 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         purple1: "#1A184E",
         blue1: "#0163FF",
         green1: "#96A630",
         bg1darkgradient: "#0f0330",
-        bg1lightgradient: "#283471"
-      }
+        bg1lightgradient: "#283471",
+        choice1: "#004CC6",
+        choice2: "#E5B50B",
+        choice3: "#90A800",
+        choice4: "#D60073",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
-export default config
