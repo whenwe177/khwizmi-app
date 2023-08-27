47d045b2970e3063d58d01713512eddca78d2bd8
Rashad Aziz <rashad.aziz@ui.ac.id>
2023-08-27 11:16:03 +1000

    jokes

--- pages/api/khwizitalk.ts
+++ pages/api/khwizitalk.ts
@@ -1,10 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import OpenAI from 'openai';
+import { jsonrepair } from "jsonrepair";
+import type { NextApiRequest, NextApiResponse } from "next";
+import OpenAI from "openai";
 
 type Data = {
-  dialogue: string
-}
+  dialogue: string;
+};
 
 const openai = new OpenAI();
 
@@ -12,23 +13,75 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const { context } = JSON.parse(req.body);
+  // const { context } = JSON.parse(req.body);
 
-  // const completion = await openai.chat.completions.create({
-  //   messages: [
-  //     {
-  //       role: "user",
-  //       content: `You are a quick witted knowledgeable comedian who can make smart jokes on the spot. Give me an array of strings consisting a maximum of 10 jokes based on the context I will send prefixed with CONTEXT:. Respond with the array ONLY; that is, make sure that you respond with this structure ["", "", ""].`,
-  //     },
-  //     {
-  //       role: "user",
-  //       content: `CONTEXT:${context}`,
-  //     },
-  //   ],
-  //   model: "gpt-3.5-turbo",
-  //   n: 1,
-  // });
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: `You are a quick witted knowledgeable comedian who can make smart jokes on the spot.`,
+      },
+      {
+        role: "user",
+        content: `CONTEXT:There is a fundamental quantity called the Fisher information matrix (Ly et al.,
+          2017). The Fisher information matrix determines the characteristics of statis-
+          tical estimation and hypothetical testing. The derivatives of log-likelihood are
+          called the score function. The Fisher information matrix is defined as the vari-
+          ance of the score function. For now, it can be said that the Fisher information is
+          something like the amount of information obtained from observations. When a
+          parameter is unidimensional (multi-dimensional), we call the Fisher information
+          (matrix).
+          The most important example which implies the importance of the Fisher in-
+          formation matrix for the present paper is that the inverse of Fisher information
+          (matrix) can predict variance (covariance matrix) of the Maximum Likelihood
+          estimator (MLE; Chang, 2015). Higher Fisher information leads to higher es-
+          timation precision (lower variance of estimation). Further, the example which
+          uses the Fisher information matrix directly in psychological field is Comput-
+          erized adaptive testing (CAT; Weiss & Kingsbury, 1984; Segall, 2004; Meijer
+          & Nering, 1999; van der Linden, 2018; van der Linden, 1998). In CAT, the
+          Fisher information (matrix) is used for selecting stimuli (e.g., quiz of an educa-
+          1
+          tional test, an item for questionnaire, reward and reward probability for decision
+          making task) adaptively and optimally.
+          Although the Fisher information matrix is a fundamental and important
+          quantity in statistics, there are few cases where it is used effectively in psyc hol-
+          ogy or psychological statistics. It will be useful for determining the experimental
+          design, especially selecting stimuli, with the optimal and objective procedures.
+          In our understanding, there are some reasons why the Fisher information
+          matrix is not often used in psychology. The first reason is that researchers do
+          not recognize the value of the Fisher information matrix. The second reason
+          is that researchers will have some hurdles in deriving its analytical solution.
+          Therefore, this paper explains why the Fisher information matrix is important,
+          especially for statistical inference in section 2. Then this paper explains how
+          to calculate the Fisher information matrix with some calculation examples in
+          section 3. In addition, this paper provides R code example in section 4`,
+      },
+      {
+        role: "user",
+        content: "Use the generate_jokes function to create an array of jokes based on the context above containing a minimum of 5 jokes"
+      },
+    ],
+    functions: [
+      {
+        name: "generate_jokes",
+        parameters: {
+          type: "object",
+          properties: {
+            jokes: {
+              type: "array",
+              description: "Array of jokes based on the context",
+              items: {
+                type: "string",
+              },
+            },
+          },
+        },
+      },
+    ],
+    model: "gpt-3.5-turbo",
+    n: 1,
+  });
 
-  // return res.status(200).json(JSON.parse(completion.choices[0].message.content || ''))
-  return res.status(200).json(JSON.parse('["Dummy Joke"]'))
+  return res.status(200).json(JSON.parse(jsonrepair(completion.choices[0].message.function_call?.arguments!)).jokes)
+  return res.status(200).json(JSON.parse('["Dummy Joke"]'));
 }
--- pages/study.tsx
+++ pages/study.tsx
@@ -2,7 +2,16 @@ import { useAppContext } from "@/context/AppContext";
 import { firestore, storage } from "@/firebase";
 import { useQuery } from "@tanstack/react-query";
 import { getBlob, ref } from "firebase/storage";
-import { Timestamp, collection, doc, getDocs, query, setDoc, updateDoc, where } from "firebase/firestore";
+import {
+  Timestamp,
+  collection,
+  doc,
+  getDocs,
+  query,
+  setDoc,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
 import { Document, Page } from "react-pdf";
@@ -23,6 +32,8 @@ import {
 } from "@/components/ui/dialog";
 import { DialogClose } from "@radix-ui/react-dialog";
 import Loading from "@/components/Loading";
+import Khwarizmi from "@/components/Khwarizmi";
+import { parsePdf } from "@/utils/pdf";
 
 const useTimer = ({ onTimerEnd }: { onTimerEnd: () => void }) => {
   const timerEnd = useRef(onTimerEnd);
@@ -99,6 +110,21 @@ const Study = () => {
     staleTime: Infinity,
   });
 
+  const jokeData = useQuery(["joke_data"], {
+    queryFn: async () => {
+      // @ts-ignore
+      const context = await parsePdf(
+        studySession.data!.data().pdf_url,
+        studySession.data!.data().pages
+      );
+      const response = await fetch("/api/khwizitalk", {
+        method: "POST",
+        body: JSON.stringify({ context }),
+      });
+      return await response.json();
+    },
+  });
+
   useEffect(() => {
     if (studySession.isSuccess) {
       const seconds = Math.floor(
@@ -109,80 +135,92 @@ const Study = () => {
   }, [studySession.isSuccess]);
 
   async function endSession() {
-    if (!studySession.data) return
+    if (!studySession.data) return;
     await updateDoc(doc(firestore, "study_session", studySession.data.id), {
-      study_end_time: Timestamp.fromDate(new Date())
-    })
-    router.push('/quiz')
+      study_end_time: Timestamp.fromDate(new Date()),
+    });
+    router.push("/quiz");
   }
 
   if (studySession.isLoading || fileData.isLoading) {
-    return (
-      <Loading/>
-    );
+    return <Loading />;
   }
 
   return (
-    <main
-      style={{
-        background: "linear-gradient(180deg, #0E032F 0%, #283472 100%)",
-      }}
-      className="min-h-screen w-full overflow-auto flex flex-col items-center lg:items-end justify-center"
-    >
-      <div
-        className="w-full h-screen fixed top-0 pointer-events-none"
-        style={{
-          background: 'url("bg1.png")',
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          backgroundPositionY: "150px",
-        }}
+    <>
+      <Khwarizmi
+        jokes={
+          jokeData.isLoading || jokeData.isError ? undefined : jokeData.data
+        }
+        posX={0}
+        posY={550}
+        width={400}
+        height={400}
       />
-      <Document
-        className={"flex flex-col items-center gap-4 p-4"}
-        loading={null}
-        file={fileData.data}
+      <main
+        style={{
+          background: "linear-gradient(180deg, #0E032F 0%, #283472 100%)",
+        }}
+        className="min-h-screen w-full overflow-auto flex flex-col items-center lg:items-end justify-center"
       >
-        {studySession.data!.data().pages.map((i: number) => (
-          <Page scale={2} loading={null} pageNumber={i} key={i} />
-        ))}
-      </Document>
-      <div className="fixed top-8 left-24 flex items-center gap-4">
-        <div className="h-[80px] w-[250px] rounded-lg bg-yellow1 text-black flex items-center justify-center">
-          <span
-            className={clsx(
-              "text-4xl font-bold",
-              seconds < 60 && "text-red-500"
-            )}
-          >
-            {`${Math.floor(seconds / 60)}`.padStart(2, "0")}:
-            {`${seconds - Math.floor(seconds / 60) * 60}`.padStart(2, "0")}
-          </span>
+        <div
+          className="w-full h-screen fixed top-0 pointer-events-none"
+          style={{
+            background: 'url("bg1.png")',
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPositionY: "150px",
+          }}
+        />
+        <Document
+          className={"flex flex-col items-center gap-4 p-4"}
+          loading={null}
+          file={fileData.data}
+        >
+          {studySession.data!.data().pages.map((i: number) => (
+            <Page scale={2} loading={null} pageNumber={i} key={i} />
+          ))}
+        </Document>
+        <div className="fixed top-8 left-24 flex items-center gap-4">
+          <div className="h-[80px] w-[250px] rounded-lg bg-yellow1 text-black flex items-center justify-center">
+            <span
+              className={clsx(
+                "text-4xl font-bold",
+                seconds < 60 && "text-red-500"
+              )}
+            >
+              {`${Math.floor(seconds / 60)}`.padStart(2, "0")}:
+              {`${seconds - Math.floor(seconds / 60) * 60}`.padStart(2, "0")}
+            </span>
+          </div>
+          <Dialog>
+            <DialogTrigger>
+              <Button variant={"destructive"}>End Session</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>You still have time!</DialogTitle>
+                <DialogDescription>
+                  You will be take to the A.I curated review quiz directly. You
+                  still have time to study! Are you sure about this?
+                </DialogDescription>
+                <DialogFooter>
+                  <DialogClose>
+                    <Button variant={"ghost"}>Nevermind</Button>
+                  </DialogClose>
+                  <Button
+                    onClick={endSession}
+                    className="bg-yellow1 hover:bg-yellow-600 text-black"
+                  >
+                    I'm ready!
+                  </Button>
+                </DialogFooter>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
         </div>
-        <Dialog>
-          <DialogTrigger>
-            <Button variant={"destructive"}>End Session</Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>You still have time!</DialogTitle>
-              <DialogDescription>
-                You will be take to the A.I curated review quiz directly. You
-                still have time to study! Are you sure about this?
-              </DialogDescription>
-              <DialogFooter>
-                <DialogClose>
-                  <Button variant={"ghost"}>Nevermind</Button>
-                </DialogClose>
-                <Button onClick={endSession} className="bg-yellow1 hover:bg-yellow-600 text-black">
-                  I'm ready!
-                </Button>
-              </DialogFooter>
-            </DialogHeader>
-          </DialogContent>
-        </Dialog>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 Study.auth = true;
