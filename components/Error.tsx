bec253c81c62bbc892b2b511302f779009b20a00
Indira Devi <indiradevi@Indiras-MacBook-Pro.local>
2023-08-27 11:14:17 +1000

    Error page

--- components/Error.tsx
+++ components/Error.tsx
@@ -1,18 +1,154 @@
 import React from 'react';
 import bg from "../public/bg1.png";
 
-const Error = () => {
-  return (
-    <div
-      style={{
-        background: `url(${bg.src}), linear-gradient(180deg, #0E032F 0%, #283472 100%)`,
-        backgroundSize: "cover",
-      }}
-      className="h-screen p-8 flex flex-col items-center gap-3 justify-center"
-    >
-        <p className="text-white text-2xl font-bold">An error occured...</p>
-    </div>
-  )
+import KhawarizmiSleeping from "@/components/KhwarizmiSleeping";
+import { motion } from 'framer-motion';
+import Star from './Star';
+import { Button } from '@/components/ui/button';
+import { useAppContext } from '@/context/AppContext';
+import { useRouter } from 'next/router';
+import Logo from "@/components/Logo";
+import { signInWithPopup } from "firebase/auth";
+import { auth, firestore, googleProvider } from "@/firebase";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+
+interface ErrorProps {
+  statusCode: number;
+  message: string;
 }
 
-export default Error
+const Stars = () => {
+  return (
+    <>
+      <motion.div
+        style={{ opacity: 0.85, x: 650, y: -200, position: "absolute" }}
+      >
+        <Star className="h-[250px] w-[250px]" />
+      </motion.div>
+      <motion.div
+        style={{
+          opacity: 0.85,
+          x: -700,
+          y: -200,
+          rotate: 90,
+          position: "absolute",
+        }}
+      >
+        <Star className="h-[150px] w-[150px]" />
+      </motion.div>
+      <motion.div
+        style={{
+          opacity: 0.85,
+          x: 250,
+          y: 300,
+          rotate: 110,
+          position: "absolute",
+        }}
+      >
+        <Star className="h-[80px] w-[80px]" />
+      </motion.div>
+      <motion.div
+        style={{
+          opacity: 0.85,
+          x: -800,
+          y: 300,
+          rotate: 50,
+          position: "absolute",
+        }}
+      >
+        <Star className="h-[80px] w-[80px]" />
+      </motion.div>
+      <motion.div
+        style={{
+          opacity: 0.85,
+          x: -700,
+          y: 400,
+          rotate: 50,
+          position: "absolute",
+        }}
+      >
+        <Star className="h-[50px] w-[50px]" />
+      </motion.div>
+      <motion.div
+        style={{
+          opacity: 0.85,
+          x: -500,
+          y: 250,
+          rotate: 50,
+          position: "absolute",
+        }}
+      >
+        <Star className="h-[30px] w-[30px]" />
+      </motion.div>
+    </>
+  );
+};
+
+const KhawarizmiSleepingMotion = () => {
+  return (
+    <>
+      <motion.div
+        style={{opacity:1, x: 500, y: 120, position: "absolute" }}
+      >
+        <KhawarizmiSleeping className="h-[480px] w-[480px]" />
+      </motion.div>
+    </>
+  );
+};
+
+
+const Error: React.FC<ErrorProps> = ({message}) => { 
+  const { user } = useAppContext();
+  const router = useRouter();
+
+  const loginWithGoogle = async () => {
+    const user = await signInWithPopup(auth, googleProvider);
+    const usersRef = doc(firestore, "user_attributes", user.user.uid);
+    const getUser = await getDoc(usersRef);
+    if (getUser.data() == null) {
+      await setDoc(usersRef, {
+        experience: 0,
+        name: user.user.displayName,
+        photo_url: user.user.photoURL
+      })
+    }
+  };
+  
+  const goBackHome = async () => {
+    if (!user) {
+      await loginWithGoogle();
+    }
+
+    router.push("/");
+  }
+  
+  return (
+  
+  <div 
+  style={{
+    background: `url(${bg.src}), linear-gradient(180deg, #0E032F 0%, #283472 100%)`,
+    backgroundSize: "cover",
+  }}
+  className="h-screen p-8 flex flex-col items-center gap-3 justify-center"
+  >
+    <div className="flex items-center gap-4 border-b-2 pb-1">
+          <Logo className="h-[32px] w-[32px]" />
+          <h1 className="text-white text-1xl font-semibold">Khwizmi</h1>
+    </div>
+    ``
+    <h1 className="text-white text-8xl font-bold">Uh oh!</h1>
+    <p className="text-white text-2xl font-bold">{message}</p>
+    <br></br>
+    <Button className="bg-yellow1 hover:bg-yellow-600 text-black font-bold" onClick={goBackHome}>
+          Go Back to Home
+    </Button>
+    <Stars />
+    <KhawarizmiSleepingMotion />
+
+  </div>
+  
+);
+};
+
+export default Error;
+
---
+++ components/KhwarizmiSleeping.tsx
@@ -0,0 +1,126 @@
+import React from "react";
+
+const KhawarizmiSleeping = ({  children, ...rest }: React.SVGAttributes<SVGElement>) => {
+  return (
+    <svg
+      {...rest}
+      viewBox="0 0 567 633"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <mask id="mask0_123_4960" maskUnits="userSpaceOnUse" x="46" y="26" width="495" height="558">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M46.1543 481.179C147.828 476.479 243.203 416.014 287.665 316.787C332.126 217.56 313.772 106.134 249.607 27.1255C291.759 25.1771 334.993 32.8135 376.13 51.2464C516.495 114.141 579.296 278.915 516.402 419.28C453.507 559.644 288.733 622.446 148.368 559.551C107.231 541.118 72.7557 513.934 46.1543 481.179Z" fill="url(#paint0_linear_123_4960)"/>
+</mask>
+<g mask="url(#mask0_123_4960)">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M46.1543 481.179C147.828 476.479 243.203 416.014 287.665 316.787C332.126 217.56 313.772 106.134 249.607 27.1255C291.759 25.1771 334.993 32.8135 376.13 51.2464C516.495 114.141 579.296 278.915 516.402 419.28C453.507 559.644 288.733 622.446 148.368 559.551C107.231 541.118 72.7557 513.934 46.1543 481.179Z" fill="url(#paint1_linear_123_4960)"/>
+<ellipse cx="492" cy="123.5" rx="39" ry="41.5" fill="url(#paint2_linear_123_4960)"/>
+<ellipse cx="157" cy="460.5" rx="39" ry="41.5" fill="url(#paint3_linear_123_4960)"/>
+<ellipse cx="487" cy="309.5" rx="39" ry="41.5" fill="url(#paint4_linear_123_4960)"/>
+<ellipse cx="251" cy="584.5" rx="39" ry="41.5" fill="url(#paint5_linear_123_4960)"/>
+<circle cx="290" cy="228" r="96" fill="#D9D9D9"/>
+<circle cx="290" cy="228" r="96" fill="url(#paint6_linear_123_4960)"/>
+<circle cx="419" cy="590" r="96" fill="#D9D9D9"/>
+<circle cx="419" cy="590" r="96" fill="url(#paint7_linear_123_4960)"/>
+<circle cx="382" cy="82" r="16" fill="#D9D9D9"/>
+<circle cx="382" cy="82" r="16" fill="url(#paint8_linear_123_4960)"/>
+<circle cx="219" cy="504" r="16" fill="#D9D9D9"/>
+<circle cx="219" cy="504" r="16" fill="url(#paint9_linear_123_4960)"/>
+<circle cx="397" cy="366" r="16" fill="#D9D9D9"/>
+<circle cx="397" cy="366" r="16" fill="url(#paint10_linear_123_4960)"/>
+</g>
+<path d="M256.426 238.703C259.423 235.94 264.261 237.048 265.756 240.84L295.095 315.222C295.585 316.463 296.488 317.498 297.651 318.152L367.365 357.31C370.92 359.306 371.361 364.249 368.217 366.844L306.541 417.732C305.512 418.582 304.807 419.76 304.545 421.068L288.846 499.471C288.045 503.468 283.481 505.415 280.042 503.227L212.585 460.295C211.46 459.579 210.121 459.272 208.796 459.428L129.379 468.725C125.33 469.199 122.068 465.459 123.087 461.512L143.071 384.091C143.405 382.798 143.283 381.431 142.726 380.218L109.342 307.561C107.64 303.857 110.189 299.599 114.258 299.348L194.065 294.43C195.397 294.348 196.66 293.809 197.641 292.905L256.426 238.703Z" fill="url(#paint11_linear_123_4960)"/>
+<path d="M234.256 249.371L338.949 317.027C337.911 326.19 333.382 344.475 323.577 344.317C311.321 344.119 267.28 307.549 260.416 299.34C254.925 292.773 243.512 287.162 238.492 285.177C223.749 282.369 179.866 304.262 174.988 303.164C170.111 302.065 207.916 243.439 234.256 249.371Z" fill="#E99C2E"/>
+<path d="M188.522 409.65C188.992 415.906 208.758 404.282 221.723 406.792C234.689 409.302 248.285 429.654 250.669 424.383" stroke="#2F1F0D" stroke-width="24" stroke-linecap="round"/>
+<path d="M240.357 413.456C222.491 425.423 208.849 413.022 204.261 405.326C191.715 392.763 170.472 386.953 163.639 405.915C158.172 421.085 167.586 422.522 172.977 421.344C163.077 454.376 185.481 461.403 197.92 460.788C201.534 486.613 215.035 475.064 221.333 466.061C220.894 468.012 223.498 471.469 237.43 469.687C251.362 467.904 255.874 449.239 256.388 440.13C268.508 450.035 271.742 437.95 274.365 430.853C276.989 423.756 262.689 398.497 240.357 413.456Z" fill="url(#paint12_linear_123_4960)"/>
+<path d="M184.057 399.886C186.178 404.123 203.432 413.989 224.508 402.335C230.291 413.067 246.574 417.384 255.212 418.476C267.477 434.562 284.527 427.127 286.478 427.567C299.661 425.924 263.526 374.733 237.757 377.643L229.671 379.408L222.148 374.126C200.114 360.451 145.523 391.208 156.728 398.344C165.693 404.053 178.683 401.751 184.057 399.886Z" fill="url(#paint13_linear_123_4960)"/>
+<path d="M263.847 338.723C269.051 333.062 290.006 331.803 296.308 347.06" stroke="#9F3B1B" stroke-width="9" stroke-linecap="round"/>
+<path d="M180.887 320.21C186.092 314.549 207.047 313.29 213.349 328.547" stroke="#9F3B1B" stroke-width="9" stroke-linecap="round"/>
+<path d="M209.807 346.476C204.377 351.922 183.388 352.33 177.711 336.83" stroke="#2F1F0D" stroke-width="9" stroke-linecap="round"/>
+<path d="M291.948 368.339C286.518 373.785 265.529 374.193 259.851 358.693" stroke="#2F1F0D" stroke-width="9" stroke-linecap="round"/>
+<path d="M312.516 412.811C310.601 384.082 320.383 328.074 325.749 313.247L357.069 326.69C354.791 334.155 349.587 368.199 346.569 399.233C342.797 438.025 381.137 463.512 362.87 463.041C344.602 462.57 328.512 491.523 305.877 488.887C283.241 486.251 314.911 448.722 312.516 412.811Z" fill="url(#paint14_linear_123_4960)"/>
+<path d="M312.516 412.811C310.601 384.082 320.383 328.074 325.749 313.247L357.069 326.69C354.791 334.155 349.587 368.199 346.569 399.233C342.797 438.025 381.137 463.512 362.87 463.041C344.602 462.57 328.512 491.523 305.877 488.887C283.241 486.251 314.911 448.722 312.516 412.811Z" fill="url(#paint15_linear_123_4960)"/>
+<path d="M311.28 236.473C331.407 232 346.871 237.254 352.087 240.441C352.7 242.336 352.601 245.686 351.993 249.976C361.231 254.894 372.907 269.264 371.023 278.316C369.138 287.367 361.954 309.585 365.796 332.062C358.286 340.688 329.127 334.301 315.486 330.029C311.002 332.397 304.749 327.362 301.162 326.803C283.758 317.602 256.838 283.853 234.914 268.222C260.562 257.092 295.734 239.928 311.28 236.473Z" fill="url(#paint16_linear_123_4960)"/>
+<path d="M310.667 238.634C330.794 234.161 347.612 247.665 352.828 250.851C363.282 236.293 312.868 201.146 290.253 190.909C267.637 180.672 230.431 201.67 208.451 205.561C186.471 209.452 189.052 222.561 166.139 230.883C143.226 239.205 156.351 254.045 150.027 288.092C152.726 317.764 178.904 303.251 196.206 296.166C201.72 296.702 218.442 277.266 234.302 270.383C259.949 259.254 295.121 242.089 310.667 238.634Z" fill="url(#paint17_linear_123_4960)"/>
+<defs>
+<linearGradient id="paint0_linear_123_4960" x1="265.5" y1="13.5" x2="431" y2="657" gradientUnits="userSpaceOnUse">
+<stop stop-color="white"/>
+<stop offset="1" stop-color="#333B70"/>
+</linearGradient>
+<linearGradient id="paint1_linear_123_4960" x1="265.5" y1="13.5" x2="431" y2="657" gradientUnits="userSpaceOnUse">
+<stop stop-color="white"/>
+<stop offset="1" stop-color="#333B70"/>
+</linearGradient>
+<linearGradient id="paint2_linear_123_4960" x1="439.5" y1="230" x2="589.5" y2="44" gradientUnits="userSpaceOnUse">
+<stop offset="0.181433" stop-color="#E5E6ED"/>
+<stop offset="0.882411" stop-color="#181449"/>
+</linearGradient>
+<linearGradient id="paint3_linear_123_4960" x1="104.5" y1="567" x2="254.5" y2="381" gradientUnits="userSpaceOnUse">
+<stop offset="0.181433" stop-color="#E5E6ED"/>
+<stop offset="0.882411" stop-color="#181449"/>
+</linearGradient>
+<linearGradient id="paint4_linear_123_4960" x1="434.5" y1="416" x2="584.5" y2="230" gradientUnits="userSpaceOnUse">
+<stop offset="0.181433" stop-color="#E5E6ED"/>
+<stop offset="0.882411" stop-color="#181449"/>
+</linearGradient>
+<linearGradient id="paint5_linear_123_4960" x1="198.5" y1="691" x2="348.5" y2="505" gradientUnits="userSpaceOnUse">
+<stop offset="0.181433" stop-color="#E5E6ED"/>
+<stop offset="0.882411" stop-color="#181449"/>
+</linearGradient>
+<linearGradient id="paint6_linear_123_4960" x1="160.769" y1="474.361" x2="502.595" y2="23.3269" gradientUnits="userSpaceOnUse">
+<stop offset="0.181433" stop-color="#E5E6ED"/>
+<stop offset="0.882411" stop-color="#181449"/>
+</linearGradient>
+<linearGradient id="paint7_linear_123_4960" x1="289.769" y1="836.361" x2="631.595" y2="385.327" gradientUnits="userSpaceOnUse">
+<stop offset="0.181433" stop-color="#E5E6ED"/>
+<stop offset="0.882411" stop-color="#181449"/>
+</linearGradient>
+<linearGradient id="paint8_linear_123_4960" x1="360.462" y1="123.06" x2="417.432" y2="47.8878" gradientUnits="userSpaceOnUse">
+<stop offset="0.181433" stop-color="#E5E6ED"/>
+<stop offset="0.882411" stop-color="#181449"/>
+</linearGradient>
+<linearGradient id="paint9_linear_123_4960" x1="197.462" y1="545.06" x2="254.432" y2="469.888" gradientUnits="userSpaceOnUse">
+<stop offset="0.181433" stop-color="#E5E6ED"/>
+<stop offset="0.882411" stop-color="#181449"/>
+</linearGradient>
+<linearGradient id="paint10_linear_123_4960" x1="375.462" y1="407.06" x2="432.432" y2="331.888" gradientUnits="userSpaceOnUse">
+<stop offset="0.181433" stop-color="#E5E6ED"/>
+<stop offset="0.882411" stop-color="#181449"/>
+</linearGradient>
+<linearGradient id="paint11_linear_123_4960" x1="172.251" y1="219.758" x2="295.224" y2="634.073" gradientUnits="userSpaceOnUse">
+<stop offset="0.0729167" stop-color="#FFDF9C"/>
+<stop offset="0.421875" stop-color="#FFBF3A"/>
+<stop offset="0.786458" stop-color="#DE6B00"/>
+</linearGradient>
+<linearGradient id="paint12_linear_123_4960" x1="222.725" y1="401.319" x2="205.82" y2="476.377" gradientUnits="userSpaceOnUse">
+<stop stop-color="#931818"/>
+<stop offset="0.5" stop-color="#9F3B1B"/>
+<stop offset="1" stop-color="#D54E23"/>
+</linearGradient>
+<linearGradient id="paint13_linear_123_4960" x1="230.201" y1="374.779" x2="220.648" y2="417.195" gradientUnits="userSpaceOnUse">
+<stop stop-color="#7D2408"/>
+<stop offset="0.279426" stop-color="#A54021"/>
+<stop offset="1" stop-color="#BE5737"/>
+</linearGradient>
+<linearGradient id="paint14_linear_123_4960" x1="347.776" y1="315.388" x2="330.671" y2="491.303" gradientUnits="userSpaceOnUse">
+<stop stop-color="#452D8D"/>
+<stop offset="0.306617" stop-color="#7D57B5"/>
+</linearGradient>
+<linearGradient id="paint15_linear_123_4960" x1="347.776" y1="315.388" x2="330.671" y2="491.303" gradientUnits="userSpaceOnUse">
+<stop stop-color="#C9A267"/>
+<stop offset="0.306617" stop-color="#ECD9B5"/>
+</linearGradient>
+<linearGradient id="paint16_linear_123_4960" x1="306.96" y1="232.502" x2="297.338" y2="331.452" gradientUnits="userSpaceOnUse">
+<stop stop-color="#D6BD8E"/>
+<stop offset="1" stop-color="#EDDAB6"/>
+</linearGradient>
+<linearGradient id="paint17_linear_123_4960" x1="234.922" y1="180.42" x2="245.056" y2="313.855" gradientUnits="userSpaceOnUse">
+<stop stop-color="#D5B785"/>
+<stop offset="0.369792" stop-color="#F0E1C3"/>
+</linearGradient>
+</defs>
+</svg>
+
+  );
+};
+
+export default KhawarizmiSleeping;
